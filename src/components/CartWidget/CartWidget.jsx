e4d2a022cef67e065abf3e74d7d16a7603ea5fa9
Claudio Cabanas <ccabanas@alonenet.com.ar>
2022-10-26 19:43:49 -0300

    primera entrega

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,63 +1,10 @@
-import React from 'react'
-import Tooltip from '@mui/material/Tooltip';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-
+import React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function CartWidget() {
-
-const settings = ['Mi Compra', '$ Total'];
-
-      const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-
-
-
   return (
     <div>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <AddShoppingCartIcon sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1 }} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-
+      <ShoppingCartIcon />
     </div>
-  )
+  );
 }
